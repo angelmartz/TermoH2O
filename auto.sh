1da2c0e983ccd12b731804f5cf4f3638f7ea9b74
Angel Martz <angel.martz24@gmail.com>
2012-10-31 08:17:18 -0600

    hola

--- auto.sh
+++ auto.sh
@@ -5,8 +5,8 @@ if test $# -lt 1; then
     echo Agregando commit "auto"
     git commit -m "auto commit"
 else
-    echo Agregando commit $1
-    git commit -m $1
+    echo Agregando commit "$1"
+    git commit -m "$1"
 fi
 echo Subiendo archivos
 git push origin master
